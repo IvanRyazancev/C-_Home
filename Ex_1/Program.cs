6f528fc57150d4673f3e76fd84a5dffabfe8d2e2
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-21 23:00:06 +0300

    Конец первого урока.

--- Ex_1/Program.cs
+++ Ex_1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Первая программа "Hello world!"
+
+/*
+Console.WriteLine("Hello world!");
+
+Console.WriteLine("Привет мир!");
+*/
+
+//==================================================
+
+//Структура проекта. Структура выполнения программы.
+
+//=================================================
+
+//Что такое переменная
+
+/*
+int a = 5, b = 10;
+Console.WriteLine(a + b);
+double c = 1.33;
+Console.WriteLine(c);
+string d = "fbvjsnkfb";
+Console.WriteLine(d);
+*/
